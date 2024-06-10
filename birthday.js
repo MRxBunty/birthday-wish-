98fdf5defb73fbb3c9d7fdecb09e32d606862252
MR x BUNTY <160619973+MRxBunty@users.noreply.github.com>
2024-06-09 21:37:02 -0600

    birthday wish

---
+++ birthday.js
@@ -0,0 +1,7 @@
+function wishHappyBirthday(name) {
+    return "Happy Birthday, ".concat(name, "! \uD83C\uDF89\uD83C\uDF82");
+}
+var birthdayPerson = "RAHEEL";
+console.log(wishHappyBirthday(birthdayPerson));
+var age = "19";
+console.log("Congratulation MR RAHEEL Now You Are " + (age) + "Year Old ");
---
+++ birthday.ts
@@ -0,0 +1,9 @@
+function wishHappyBirthday(name: string): string {
+    return `Happy Birthday, ${name}! 🎉🎂`;
+}
+
+const birthdayPerson = "RAHEEL";
+console.log(wishHappyBirthday(birthdayPerson));
+
+let age = "19"
+console.log(`Congratulation MR RAHEEL Now You Are `+(age)+`Year Old `);
